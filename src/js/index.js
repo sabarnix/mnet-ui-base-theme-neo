885c1632b5c3027bc4a59133a4645ad76460b672
varun.muk@media.net <varun.muk@media.net>
2023-06-13 12:07:52 +0530

    Fix comma

--- src/js/index.js
+++ src/js/index.js
@@ -2605,7 +2605,7 @@ export const generate = (baseSpacing = 24, scale = 6) => {
                 border: `1px solid ${normalizeColor('light-21', theme)}`,
               },
             }),
-          }
+          },
         },
         tag: {
           label: {
