fa797c1df26c9f3a63fcd5527394f9025c5e48c2
varun.muk@media.net <varun.muk@media.net>
2023-06-20 17:27:02 +0530

    Theme changes for multiselect

--- dist/es6/index.js
+++ dist/es6/index.js
@@ -1565,6 +1565,16 @@ export var generate = function generate(baseSpacing, scale) {
           }
         }
       },
+      normalBtn: {
+        primary: false,
+        style: {
+          background: 'white',
+          flexGrow: 1,
+          height: '100%'
+        },
+        showIcon: true,
+        color: 'dark-7'
+      },
       includeBtn: {
         primary: false,
         style: {
