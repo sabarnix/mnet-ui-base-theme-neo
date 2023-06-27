c65d1af9cdab7aff38fb4fb1eb3a1f1386e55f89
varun.muk@media.net <varun.muk@media.net>
2023-06-27 14:54:20 +0530

    Filter Selection Checkbox Theme

--- dist/es6/index.js
+++ dist/es6/index.js
@@ -2925,20 +2925,16 @@ export var generate = function generate(baseSpacing, scale) {
                 gap: baseSpacing / 2 + "px",
                 align: 'center',
                 margin: '0',
+                pad: baseSpacing * 0.5 + "px " + baseSpacing * 0.75 + "px",
+                label: {
+                  margin: {
+                    left: baseSpacing * 0.25 + "px"
+                  },
+                  size: baseSpacing * 0.875 + "px"
+                },
                 extend: function extend(_ref24) {
                   var theme = _ref24.theme;
                   return {
-                    fontSize: baseSpacing * 0.875 + "px",
-                    '> label': {
-                      width: '100%',
-                      padding: baseSpacing * 0.5 + "px " + baseSpacing * 0.75 + "px",
-                      '> div': {
-                        marginRight: baseSpacing * 0.5 + "px",
-                        '> span': {
-                          fontSize: baseSpacing * 0.875 + "px"
-                        }
-                      }
-                    },
                     '&:hover': {
                       cursor: 'pointer',
                       background: normalizeColor('active', theme)
